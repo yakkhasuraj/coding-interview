9ece20303d3cd305cd62cae5db4cd3834604ba6b
Suraj Yakkha <yakkhasuraj@gmail.com>
2024-05-11 22:51:29 +0545

    feat: reverse an integer

---
+++ 03-reverse-int/index.js
@@ -0,0 +1,14 @@
+// Given an integer, return an integer that is the reverse ordering of numbers.
+
+// Examples
+// reverseInt(15) === 51;
+// reverseInt(-15) === -51
+
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
+
+console.log(reverseInt(15));
+console.log(reverseInt(-15));
