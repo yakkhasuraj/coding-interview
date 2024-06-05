fa561a6c6536e6fb40b5a0c6cf4fb8c308ab5935
Suraj Yakkha <yakkhasuraj@gmail.com>
2024-06-05 19:54:40 +0545

    feat: implement merge sort

--- 27-sorting/index.js
+++ 27-sorting/index.js
@@ -32,6 +32,33 @@ function selectionSort(arr) {
   return arr;
 }
 
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
+
 const array = [2, 0, 4, 3, 1, 5];
 console.log(bubbleSort(array));
 console.log(selectionSort(array));
+console.log(mergeSort(array));
